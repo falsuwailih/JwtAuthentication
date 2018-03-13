e3e55ab0408cdad3b4230b5833d0b46954eb3cbb
falsuwailih <falsuwailih@Fahad-Laptop>
2018-03-13 17:30:24 +0300

    init

---
+++ src/com/falsuwailih/jwtauthentication/rest/EchoEndpoint.java
@@ -0,0 +1,44 @@
+package com.falsuwailih.jwtauthentication.rest;
+
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+@Path("/echo")
+@Produces(TEXT_PLAIN)
+public class EchoEndpoint {
+
+    // ======================================
+    // =          Injection Points          =
+    // ======================================
+
+   /* @Inject
+    private Logger logger;*/
+
+    // ======================================
+    // =          Business methods          =
+    // ======================================
+
+    @GET
+    public Response echo(@QueryParam("message") String message) {
+        return Response.ok().entity(message == null ? "no message" : message).build();
+    }
+
+    @GET
+    @Path("jwt")
+    @JWTTokenNeeded
+    public Response echoWithJWTToken(@QueryParam("message") String message) {
+        return Response.ok().entity(message == null ? "no message" : message).build();
+    }
+}
---
+++ src/com/falsuwailih/jwtauthentication/rest/UserEndpoint.java
@@ -0,0 +1,132 @@
+package com.falsuwailih.jwtauthentication.rest;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.TextCodec;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import java.security.Key;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
+import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
+
+/**
+ * @author Antonio Goncalves http://www.antoniogoncalves.org --
+ */
+@Path("/users")
+@Produces(APPLICATION_JSON)
+@Consumes(APPLICATION_JSON)
+@Transactional
+public class UserEndpoint {
+
+	// ======================================
+	// = Injection Points =
+	// ======================================
+
+	@Context
+	private UriInfo uriInfo;
+
+	/*
+	 * @Inject private Logger logger;
+	 */
+
+	@PersistenceContext
+	private EntityManager em;
+
+	// ======================================
+	// = Business methods =
+	// ======================================
+
+	@POST
+	@Path("/login")
+	@Consumes(APPLICATION_FORM_URLENCODED)
+	public Response authenticateUser(@FormParam("login") String login, @FormParam("password") String password) {
+
+		try {
+
+			// logger.info("#### login/password : " + login + "/" + password);
+
+			// Authenticate the user using the credentials provided
+			authenticate(login, password);
+
+			// Issue a token for the user
+			String token = issueToken(login);
+
+			// Return the token on the response
+			return Response.ok().header(AUTHORIZATION, "Bearer " + token).build();
+
+		} catch (Exception e) {
+			return Response.status(UNAUTHORIZED).build();
+		}
+	}
+
+	private void authenticate(String login, String password) throws Exception {
+
+	}
+
+	private String issueToken(String login) {
+        String jwtToken = Jwts.builder()
+                .setSubject(login)
+                .setIssuer(uriInfo.getAbsolutePath().toString())
+                .setIssuedAt(new Date())
+                .setExpiration(toDate(LocalDateTime.now().plusMinutes(15L)))
+                .signWith(
+    				    SignatureAlgorithm.HS256,
+    				    TextCodec.BASE64.decode("Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=")
+    				  )
+                .compact();
+        //logger.info("#### generating token for a key : " + jwtToken + " - " + key);
+        return jwtToken;
+
+    }
+
+	/*
+	 * @POST public Response create(User user) { em.persist(user); return
+	 * Response.created(uriInfo.getAbsolutePathBuilder().path(user.getId()).
+	 * build()).build(); }
+	 * 
+	 * @GET
+	 * 
+	 * @Path("/{id}") public Response findById(@PathParam("id") String id) {
+	 * User user = em.find(User.class, id);
+	 * 
+	 * if (user == null) return Response.status(NOT_FOUND).build();
+	 * 
+	 * return Response.ok(user).build(); }
+	 * 
+	 * @GET public Response findAllUsers() { TypedQuery<User> query =
+	 * em.createNamedQuery(User.FIND_ALL, User.class); List<User> allUsers =
+	 * query.getResultList();
+	 * 
+	 * if (allUsers == null) return Response.status(NOT_FOUND).build();
+	 * 
+	 * return Response.ok(allUsers).build(); }
+	 * 
+	 * @DELETE
+	 * 
+	 * @Path("/{id}") public Response remove(@PathParam("id") String id) {
+	 * em.remove(em.getReference(User.class, id)); return
+	 * Response.noContent().build(); }
+	 */
+
+	// ======================================
+	// = Private methods =
+	// ======================================
+
+	private Date toDate(LocalDateTime localDateTime) {
+		return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+	}
+}
